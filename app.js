9847e230c44e86f52f7c8756edcbd631f2ac548a
smuliz <samuli.lindroos93@gmail.com>
2019-11-21 10:34:36 +0200

    ensimmäinen osuus tehty

--- app.js
+++ app.js
@@ -1,59 +1,71 @@
-'use strict';
-const express = require('express');
-const session = require('express-session');
+"use strict";
+const express = require("express");
+const session = require("express-session");
+const passport = require("./utils/pass");
 const app = express();
 const port = 3000;
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/form");
+  }
+};
+
+app.set("views", "./views");
+app.set("view engine", "pug");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-const username = 'foo';
-const password = 'bar';
+const username = "foo";
+const password = "bar";
 
-app.use(session({
-  secret: 'salainen kissa',
-  resave: false,
-  saveUnitialized: true,
-}));
+app.use(
+  session({
+    secret: "salainen kissa",
+    resave: false,
+    saveUnitialized: true
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-app.get('/', (req, res) => {
-  res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/setCookie/:clr', (req, res) => {
-  res.cookie('color', req.params.clr).send('cookie set')
+app.get("/setCookie/:clr", (req, res) => {
+  res.cookie("color", req.params.clr).send("cookie set");
 });
 
-app.get('deleteCookie', (req, res) => {
-  res.clearCookie('color');
-  res.send('Cookie deleted');
+app.get("deleteCookie", (req, res) => {
+  res.clearCookie("color");
+  res.send("Cookie deleted");
 });
 
-app.get('/form', (req,res) => {
-  res.render('form');
+app.get("/form", (req, res) => {
+  res.render("form");
 });
 
-app.get('/secret', (req,res) => {
-  if ( req.session.logged) {
-    res.render('secret');
-  } else {
-    res.redirect('/form');
+app.get("/secret", loggedIn, (req, res) => {
+  res.render("secret");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/form" }),
+  (req, res) => {
+    console.log("success");
+    res.redirect("/secret");
   }
-});
+);
 
-app.post('/login', (req,res) => {
-  if (username === req.body.username && password == req.body.password) {
-    req.session.logged = true;
-    res.redirect('/secret');
-  } else {
-  req.session.logged = false;
-  res.redirect('/form');
-  }
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
